e8970d98c0cfff88dafe4f82189b5217e7cebef9
Aksingh098 <reheat8725@gmail.com>
2026-06-09 14:35:34 +0530

    created DAO

---
+++ app/src/main/java/com/example/busschedule/data/BusScheduleDao.kt
@@ -0,0 +1,18 @@
+package com.example.busschedule.data
+
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+
+
+
+@Dao
+interface BusScheduleDao {
+
+    @Query("SELECT * from BusSchedule WHERE  stopName = :stopName")
+    fun getItem(stopName: String): Flow<BusSchedule>
+
+    @Query("SELECT * from BusSchedule ORDER BY stopName ASC")
+    fun getAllItems(): Flow<List<BusSchedule>>
+}
